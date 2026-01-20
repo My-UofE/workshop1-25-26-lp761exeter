4375cada89c07537d60bdea708f68bb9bfd421b1
lp761exeter <lp761@exeter.ac.uk>
2026-01-20 14:13:34 +0000

    committed mdkmdk

--- game_functions.py
+++ game_functions.py
@@ -2,16 +2,7 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    length = len(poss_values)
-    choice = 0
-    if length%2==0:
-        index = int(length/2)
-        choice = poss_values[index]
-    else:
-        index = int((length-1)/2)
-        choice = poss_values[index]
-    
-    return choice
+    return poss_values[len(poss_values)//2]
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
